dbe3965a06bfb8490045f86ced731cdfb2d8f371
Krisztian Pocza <kpocza@kpocza.net>
2020-01-10 14:51:55 +0100

    circulator service fix

--- CirculatorServer/app.py
+++ CirculatorServer/app.py
@@ -4,26 +4,26 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-endTime = datetime().utcnow()
+endTime = datetime.utcnow()
 
 @app.route('/info', methods=['GET'])
 def info():
 	global endTime
-	return "1" if datetime().utcnow() < endTime else "0"
+	return "1" if datetime.utcnow() < endTime else "0"
 
 @app.route('/short', methods=['GET'])
-def fullInfo():
+def shortCirculation():
 	setEnd(15)
 	return "OK"
 
 @app.route('/long', methods=['GET'])
-def fullInfo():
+def longCirculation():
 	setEnd(30)
 	return "OK"
 
 def setEnd(minutes):
 	global endTime
-	endTime = datetime().utcnow() + timedelta(minutes = minutes)
+	endTime = datetime.utcnow() + timedelta(minutes = minutes)
 
 if __name__ == '__main__':
 	app.run(debug=True,host="0.0.0.0",port=5001)
